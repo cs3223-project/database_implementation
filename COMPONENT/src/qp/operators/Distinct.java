d39be45677fce1b749abaf25b29c9af4c60e04bb
itszp <tayzp@u.nus.edu>
2020-04-01 00:22:53 +0800

    distinct structures

---
+++ COMPONENT/src/qp/operators/Distinct.java
@@ -0,0 +1,88 @@
+package qp.operators;
+
+import qp.optimizer.BufferManager;
+import qp.utils.Attribute;
+import qp.utils.Batch;
+import qp.utils.Condition;
+import qp.utils.Tuple;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Distinct extends Operator {
+
+    static int filenum = 0;         // To get unique filenum for this operation
+    int batchsize;                  // Number of tuples per out batch
+    int blockSize;
+    int leftindex;                  // Indices of the join attributes in left table
+    int rightindex;                 // Indices of the join attributes in right table
+    String rfname;                  // The file name where the right table is materialized
+    Batch outbatch;                 // Buffer page for output
+    List<Batch> leftbatch;          // Buffer page for left input stream
+    List<Tuple> lTuplesInCurrBlk;   //Tuples inside block
+    Batch rightbatch;               // Buffer page for right input stream
+    ObjectInputStream in;           // File pointer to the right hand materialized file
+
+    int lcurs;                      // Cursor for left side buffer
+    int rcurs;                      // Cursor for right side buffer
+    boolean eosl;                   // Whether end of stream (left table) is reached
+    boolean eosr;                   // Whether end of stream (right table) is reached
+
+    /*static int filenum = 0;         // To get unique filenum for this operation
+    int blockSize;
+    int batchsize;                  // Number of tuples per out batch
+    ArrayList<Integer> leftindex;   // Indices of the join attributes in left table
+    ArrayList<Integer> rightindex;  // Indices of the join attributes in right table
+    String rfname;                  // The file name where the right table is materialized
+    Batch outbatch;                 // Buffer page for output
+    Batch leftbatch;                // Buffer page for left input stream
+    Batch rightbatch;               // Buffer page for right input stream
+    ObjectInputStream in;           // File pointer to the right hand materialized file
+
+    int lcurs;                      // Cursor for left side buffer
+    int rcurs;                      // Cursor for right side buffer
+    boolean eosl;                   // Whether end of stream (left table) is reached
+    boolean eosr;                   // Whether end of stream (right table) is reached*/
+
+    public Distinct(Operator base, int type) {
+        super(type);
+    }
+
+    //Setter and Getter
+    public void setBlockSize(int size) {
+        this.blockSize = size;
+    }
+
+    public int getBlockSize() {
+        return blockSize;
+    }
+
+
+    /**
+     * During open finds the index of the join attributes
+     * * Materializes the right hand side into a file
+     * * Opens the connections
+     **/
+    public boolean open() {
+        return true;
+    }
+
+    /**
+     * from input buffers selects the tuples satisfying join condition
+     * * And returns a page of output tuples
+     **/
+    public Batch next() {
+        Batch batch = null;
+        return batch;
+    }
+
+    /**
+     * Close the operator
+     */
+    public boolean close() {
+        File f = new File(rfname);
+        f.delete();
+        return true;
+    }
+}
