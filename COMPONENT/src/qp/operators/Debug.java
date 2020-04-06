e7eebef36c37430d1cc4171c27a293326e4fdc82
itszp <tayzp@u.nus.edu>
2020-04-06 15:39:29 +0800

    Distinct print values:

--- COMPONENT/src/qp/operators/Debug.java
+++ COMPONENT/src/qp/operators/Debug.java
@@ -115,7 +115,12 @@ public class Debug {
             PPrint(((Project) node).getBase());
             System.out.print(")");
 
-        } else if (optype == OpType.SCAN) {
+        } else if (optype == OpType.DISTINCT) {
+            System.out.print("Distinct(");
+            PPrint(((Distinct) node).getBase());
+            System.out.print(")");
+        }
+        else if (optype == OpType.SCAN) {
             System.out.print(((Scan) node).getTabName());
         }
     }
